63d3eb810e6bd1406d55085e4d22fd63dac12190
aiyoudiao <newdiao@163.com>
2022-11-06 23:13:16 +0800

    feat: The complementary initialization migrates the component libraries I wrapped earlier from umaso 补充初始化，从umaso中迁移出我之前封 装的组件库

---
+++ .umirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "dumi";
+
+export default defineConfig({
+  title: "umaso",
+  favicon: "https://cdn.jsdelivr.net/gh/shengxinjing/static/pangxie.png",
+  logo: "https://cdn.jsdelivr.net/gh/shengxinjing/static/pangxie.png",
+  outputPath: "docs-dist",
+  mode: "site",
+  // more config: https://d.umijs.org/config
+});
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export {
+  ElHeader,
+  ElAside,
+  ElMain,
+  ElFooter,
+  ElContainer,
+} from './components/container/'
+export { ElRow, ElCol } from './components/layout/'
